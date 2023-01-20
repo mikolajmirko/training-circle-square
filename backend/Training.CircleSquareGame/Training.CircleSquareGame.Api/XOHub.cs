435e0e01791c69b6dc011eab27bfe6936baad758
Mikolaj Mirko <mikolaj.mirko@volue.com>
2023-01-20 16:54:39 +0100

    Logic implementation, frontend changes

--- backend/Training.CircleSquareGame/Training.CircleSquareGame.Api/XOHub.cs
+++ backend/Training.CircleSquareGame/Training.CircleSquareGame.Api/XOHub.cs
@@ -2,15 +2,110 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace Training.CircleSquareGame.Api;
 
+public enum Field
+{
+    Empty,
+    X,
+    O
+}
+public enum Status
+{
+    InGame,
+    WonByX,
+    WonByO,
+    Draw
+}
+
 public class XOHub : Hub
 {
-    public async Task GetField(string fieldId)
+
+    public const int BoardSize = 3;
+    public static List<List<Field>> State = new();
+    public static Field CurrentTurn = Field.O;
+    public static Status CurrentStatus = Status.InGame;
+    
+    public async Task ResetBoard()
     {
-        await Clients.All.SendAsync("CurrentFieldValue", fieldId, "");
+        CurrentTurn = Field.O;
+        CurrentStatus = Status.InGame;
+        State.Clear();
+        for (var i = 0; i < BoardSize; i++)
+        {
+            var row = new List<Field>();
+            for (var j = 0; j < BoardSize; j++)
+            {
+                row.Add(Field.Empty);
+            }
+            State.Add(row);
+        }
+        await Clients.All.SendAsync("NewGame");
+        await Clients.All.SendAsync("GameStatus", CurrentStatus);
+        await Clients.All.SendAsync("CurrentTurn", CurrentTurn);
+    }
+
+    public async Task GetCurrentTurn()
+    {
+        await Clients.All.SendAsync("CurrentTurn", CurrentTurn);
+    }
+
+    public async Task GetField(int x, int y)
+    {
+        if (x is >= 0 and < BoardSize && y is >= 0 and < BoardSize)
+        {
+            await Clients.All.SendAsync("CurrentFieldValue", x, y, State[x][y]);
+        }
     }
     
-    public async Task SetField(string fieldId)
+    public async Task SetField(int x, int y)
     {
-        await Clients.All.SendAsync("CurrentFieldValue", fieldId, "x");
+        if (x is >= 0 and < BoardSize && y is >= 0 and < BoardSize)
+        {
+            if (State[x][y] == Field.Empty && CurrentStatus == Status.InGame)
+            {
+                State[x][y] = CurrentTurn;
+                CheckWinCondition();
+                CurrentTurn = CurrentTurn == Field.O ? Field.X : Field.O;
+                await Clients.All.SendAsync("CurrentTurn", CurrentTurn);
+                await Clients.All.SendAsync("CurrentFieldValue", x, y, State[x][y]);
+            }
+        }
     }
+    
+    public async void CheckWinCondition()
+    {
+        List<List<Field>> checkLines = new();
+        List<Field> diagonalForward = new();
+        List<Field> diagonalBackward = new();
+        for (var i = 0; i < BoardSize; i++)
+        {
+            var row = State[i];
+            checkLines.Add(row);
+
+            var column = State.Select(r => r[i]).ToList();
+            checkLines.Add(column);
+            
+            diagonalForward.Add(State[i][i]);
+            diagonalBackward.Add(State[i][BoardSize-i-1]);
+        }
+        checkLines.Add(diagonalForward);
+        checkLines.Add(diagonalBackward);
+
+        foreach (var line in checkLines)
+        {
+            if (line.TrueForAll(field => field != Field.Empty && field == line[0]))
+            {
+                CurrentStatus = line[0] == Field.O ? Status.WonByO : Status.WonByX;
+                await Clients.All.SendAsync("GameStatus", CurrentStatus);
+                return;
+            }
+        }
+
+        if (State.TrueForAll(row => row.TrueForAll(field => field != Field.Empty)))
+        {
+            CurrentStatus = Status.Draw;
+            await Clients.All.SendAsync("GameStatus", CurrentStatus);
+        }
+
+    }
+
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,15 @@
-import '@riotjs/hot-reload'
-import { mount, register, install } from 'riot'
-import Hub from './hub'
-import GameBoard from './components/game-board/game-board.riot'
-import OxField from './components/ox-field/ox-field.riot'
-import ExampleButton from './components/example-button/example-button.riot'
-import ExampleText from './components/example-text/example-text.riot'
+import '@riotjs/hot-reload';
+import { mount, register, install } from 'riot';
+import Hub from './hub';
+
+import GameBoard from './components/game-board/game-board.riot';
+import OxField from './components/ox-field/ox-field.riot';
+import NewGameButton from './components/new-game-button/new-game-button.riot';
+import HeaderText from './components/header-text/header-text.riot';
 
 var appState = {
-    oxFields: {}
+    boardSize: 3,
+    board: []
 };
 
 // SignalR
@@ -16,9 +18,25 @@ var hub = new Hub("http://localhost:5000/xohub");
 // ----------------------------------
 // SignalR calls from backend go here
 
-hub.connection.on("CurrentFieldValue", (fieldId, value) => {
-    appState.oxFields[fieldId].update({value})
-})
+hub.connection.on("CurrentFieldValue", (x, y, value) => {
+    appState.board[x][y].update({value});
+});
+
+hub.connection.on("NewGame", () => {
+    for (let i = 0; i < appState.boardSize; i++) {
+        for (let j = 0; j < appState.boardSize; j++) {
+            appState.board[i][j].update({value: 0});
+        }
+    }
+});
+
+hub.connection.on("CurrentTurn", (value) => {
+    appState.header.update({currentTurn: value});
+});
+
+hub.connection.on("GameStatus", (value) => {
+    appState.header.update({currentStatus: value});
+});
 
 // SignalR calls from backend go here
 // ----------------------------------
@@ -27,22 +45,29 @@ hub.connection.on("CurrentFieldValue", (fieldId, value) => {
 install(function(component) {
     component.appState = appState;
     component.hub = hub.connection;
-})
+});
 
 // -----------------------------------------------
 // RiotJs component registration happens here here
 
-register('ox-field', OxField)
-register('game-board', GameBoard)
-register('example-button', ExampleButton)
-register('example-text', ExampleText)
+register('ox-field', OxField);
+register('game-board', GameBoard);
+register('new-game-button', NewGameButton);
+register('header-text', HeaderText);
 
 // RiotJs component registration happens here here
 // -----------------------------------------------
 
 async function StartApp() {
-    await hub.start()
-    mount('game-board')
+    for (let i = 0; i < appState.boardSize; i++) {
+        const row = [];
+        for (let j = 0; j < appState.boardSize; j++) {
+            row.push(0);
+        }
+        appState.board.push(row);
+    }
+    await hub.start();
+    mount('game-board');
 }
 
-StartApp()
+StartApp();
